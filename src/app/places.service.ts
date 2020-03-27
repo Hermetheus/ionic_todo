294247dc87d3750934cfd0b09bde9a73e1a06924
Hermetheus <allanmolson@gmail.com>
2020-03-26 19:52:28 -0500

    Fetching Data from Firebase!

--- src/app/places.service.ts
+++ src/app/places.service.ts
@@ -3,55 +3,100 @@ import { AuthService } from './auth/auth.service';
 import { Place } from './places/place.module';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-import { take, map, tap, delay } from 'rxjs/operators';
+import { take, map, tap, delay, switchMap } from 'rxjs/operators';
+
+// [
+//   // tslint:disable-next-line: max-line-length
+//   new Place(
+//     'p1',
+//     'Manhattan Mansion',
+//     'In the heart of New York City.',
+//     'https://image.shutterstock.com/image-illustration/expensive-private-villa-swimming-pool-260nw-1070624348.jpg',
+//     149.99,
+//     new Date('2019-01-01'),
+//     new Date('2019-12-31'),
+//     'xyz'
+//   ),
+//   // tslint:disable-next-line: max-line-length
+//   new Place(
+//     'p2',
+//     'Trailor',
+//     'Some place',
+//     // tslint:disable-next-line: max-line-length
+// tslint:disable-next-line: max-line-length
+//     'https://storage.bhs.cloud.ovh.net/v1/AUTH_e7d15450bedd40b9b599e075527df3cb/loz/fOLDER_MOBILE_HOME__Trailer_House__1500_S_5b344eac05591.jpg',
+//     49.99,
+//     new Date('2019-01-01'),
+//     new Date('2019-12-31'),
+//     'abc'
+//   ),
+//   // tslint:disable-next-line: max-line-length
+//   new Place(
+//     'p3',
+//     'Foggy Palace',
+//     'Not your average trip!',
+//     'https://cdnb.artstation.com/p/assets/images/images/018/412/489/4k/fernando-fleury-project-concept-v7.jpg?1559265146',
+//     99.99,
+//     new Date('2019-01-01'),
+//     new Date('2019-12-31'),
+//     'abc'
+//   )
+// ]
+interface PlaceData {
+  availableFrom: string;
+  availableTo: string;
+  description: string;
+  imageUrl: string;
+  price: number;
+  title: string;
+  userId: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlacesService {
   // tslint:disable-next-line: variable-name
-  private _places = new BehaviorSubject<Place[]>([
-    // tslint:disable-next-line: max-line-length
-    new Place(
-      'p1',
-      'Manhattan Mansion',
-      'In the heart of New York City.',
-      'https://image.shutterstock.com/image-illustration/expensive-private-villa-swimming-pool-260nw-1070624348.jpg',
-      149.99,
-      new Date('2019-01-01'),
-      new Date('2019-12-31'),
-      'xyz'
-    ),
-    // tslint:disable-next-line: max-line-length
-    new Place(
-      'p2',
-      'Trailor',
-      'Some place',
-      // tslint:disable-next-line: max-line-length
-      'https://storage.bhs.cloud.ovh.net/v1/AUTH_e7d15450bedd40b9b599e075527df3cb/loz/fOLDER_MOBILE_HOME__Trailer_House__1500_S_5b344eac05591.jpg',
-      49.99,
-      new Date('2019-01-01'),
-      new Date('2019-12-31'),
-      'abc'
-    ),
-    // tslint:disable-next-line: max-line-length
-    new Place(
-      'p3',
-      'Foggy Palace',
-      'Not your average trip!',
-      'https://cdnb.artstation.com/p/assets/images/images/018/412/489/4k/fernando-fleury-project-concept-v7.jpg?1559265146',
-      99.99,
-      new Date('2019-01-01'),
-      new Date('2019-12-31'),
-      'abc'
-    )
-  ]);
+  private _places = new BehaviorSubject<Place[]>([]);
 
   get places() {
     return this._places.asObservable();
   }
 
   constructor(private authService: AuthService, private http: HttpClient) {}
+
+  fetchPlaces() {
+    return this.http
+      .get<{ [key: string]: PlaceData }>(
+        'https://ionic-booking-app-a32f9.firebaseio.com/offered-places.json'
+      )
+      .pipe(
+        map(resData => {
+          const places = [];
+          for (const key in resData) {
+            if (resData.hasOwnProperty(key)) {
+              places.push(
+                new Place(
+                  key,
+                  resData[key].title,
+                  resData[key].description,
+                  resData[key].imageUrl,
+                  resData[key].price,
+                  new Date(resData[key].availableFrom),
+                  new Date(resData[key].availableTo),
+                  resData[key].userId
+                )
+              );
+            }
+          }
+          return places;
+          // return [];
+        }),
+        tap(places => {
+          this._places.next(places);
+        })
+      );
+  }
 
   getPlace(id: string) {
     return this.places.pipe(
@@ -69,6 +114,7 @@ export class PlacesService {
     dateFrom: Date,
     dateTo: Date
   ) {
+    let generatedId: string;
     const newPlace = new Place(
       Math.random().toString(),
       title,
@@ -80,13 +126,19 @@ export class PlacesService {
       this.authService.userId
     );
     return this.http
-      .post(
+      .post<{ name: string }>(
         'https://ionic-booking-app-a32f9.firebaseio.com/offered-places.json',
         { ...newPlace, id: null }
       )
       .pipe(
-        tap(resData => {
-          console.log(resData);
+        switchMap(resData => {
+          generatedId = resData.name;
+          return this.places;
+        }),
+        take(1),
+        tap(places => {
+          newPlace.id = generatedId;
+          this._places.next(places.concat(newPlace));
         })
       );
     // return this.places.pipe(
