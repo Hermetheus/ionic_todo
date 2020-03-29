c6dd1b1e5e060e27e6ecf7ffe3ca31648ab43535
Hermetheus <allanmolson@gmail.com>
2020-03-29 11:59:55 -0500

    Working with Google Maps JS SDK & Deleted Bookings

---
+++ src/app/shared/map-modal/map-modal.component.ts
@@ -0,0 +1,62 @@
+import { ModalController } from '@ionic/angular';
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  ViewChild,
+  ElementRef
+} from '@angular/core';
+
+@Component({
+  selector: 'app-map-modal',
+  templateUrl: './map-modal.component.html',
+  styleUrls: ['./map-modal.component.scss']
+})
+export class MapModalComponent implements OnInit, AfterViewInit {
+  @ViewChild('map', { static: false }) mapElementRef: ElementRef;
+  constructor(private modalCtrl: ModalController) {}
+
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    this.getGoogleMaps()
+      .then(googleMaps => {
+        const mapEl = this.mapElementRef.nativeElement;
+        const map = new googleMaps.Map(mapEl, {
+          center: { lat: -34.397, lng: 150.644 },
+          zoom: 16
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  onCancel() {
+    this.modalCtrl.dismiss();
+  }
+
+  private getGoogleMaps(): Promise<any> {
+    const win = window as any;
+    const googleModule = win.google;
+    if (googleModule && googleModule.maps) {
+      return Promise.resolve(googleModule.maps);
+    }
+    return new Promise((resolve, reject) => {
+      const script = document.createElement('script');
+      script.src =
+        'https://maps.googleapis.com/maps/api/js?key=AIzaSyAT31U-a8cLemf4QB6eJZSEJspLXO6_-PQ';
+      script.async = true;
+      script.defer = true;
+      document.body.appendChild(script);
+      script.onload = () => {
+        const loadedGoogleModule = win.google;
+        if (loadedGoogleModule && loadedGoogleModule.maps) {
+          resolve(loadedGoogleModule.maps);
+        } else {
+          reject('Google Maps SDK not available.');
+        }
+      };
+    });
+  }
+}
---
+++ src/app/shared/pickers/location-picker/location-picker.component.ts
@@ -0,0 +1,20 @@
+import { MapModalComponent } from './../../map-modal/map-modal.component';
+import { ModalController } from '@ionic/angular';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-location-picker',
+  templateUrl: './location-picker.component.html',
+  styleUrls: ['./location-picker.component.scss']
+})
+export class LocationPickerComponent implements OnInit {
+  constructor(private modalCtrl: ModalController) {}
+
+  ngOnInit() {}
+
+  onPickLocation() {
+    this.modalCtrl.create({ component: MapModalComponent }).then(modalEl => {
+      modalEl.present();
+    });
+  }
+}
